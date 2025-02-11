f99f4ac27fed16ce73bcc821b58c54ba84c465fd
zksu <565338107@qq.com>
2025-02-11 17:51:18 +0800

    chore: 对于普通函数不代理.

--- packages/core/src/utils/markShallow.ts
+++ packages/core/src/utils/markShallow.ts
@@ -1,7 +1,19 @@
-import { markRaw } from './markRaw';
+import { computed } from "../computed";
+import { isComputedDescriptorParameter } from "./isComputedDescriptorParameter";
+import { markRaw } from "./markRaw";
 
 export function markShallow(root: any) {
   Object.keys(root).forEach(([key]) => {
+    markRaw(root[key]);
+  });
+  // 对于函数
+  const propertyNames = Object.getOwnPropertyNames(Object.getPrototypeOf(root));
+  propertyNames.forEach((key) => {
+    // 计算属性需要代理.
+    if (isComputedDescriptorParameter(root[key])) {
+      return;
+    }
+    // 其他普通函数不代理
     markRaw(root[key]);
   });
   return root;
